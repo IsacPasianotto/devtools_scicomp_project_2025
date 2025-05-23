450d93bbf9e5e705668a613a8267fa7cb33bc49e
Niccolo Tosato <niccolo.tosato@yahoo.it>
2025-05-23 12:33:02 +0200

    Try to run a first test

--- test/test_.py
+++ test/test_.py
@@ -1,18 +1,25 @@
 import pytest
 import pymatmatmul.gen_matrices
+import pymatmatmul.matmul
+
 import numpy as np
 
 @pytest.mark.mpi_skip()
 def test_generation_matrices():
-    A,B = pymatmatmul.gen_matrices.generate_random_matrices( n = 3,m= 4,p= 5,
+    A = pymatmatmul.gen_matrices.generate_random_matrices( n = 3,m= 4,
                                  generationMin = 0.0,generationMax = 1.0,
                                  dtype= np.float32)
-    assert A.shape == (3, 4) and B.shape == (4, 5)
+    assert A.shape == (3, 4)
 
 @pytest.mark.mpi(min_size=2)
 def test_multiple_prints():
     print("aaaa")
     assert True
+@pytest.mark.mpi(min_size=2,max_size=2)
+def test_mat_mul():
+    A = np.empty((3,4))
+    B = np.empty((4,6))
+    c = pymatmatmul.matmul.matmul(A,B,6,4,6)
 
 @pytest.mark.mpi_skip()
 def test_single_print():
