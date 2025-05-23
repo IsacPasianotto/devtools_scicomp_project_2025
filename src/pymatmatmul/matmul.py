5f1ebe6be21a287d9933524193717d4b17ed0f7b
Niccolo Tosato <niccolo.tosato@yahoo.it>
2025-05-23 15:27:28 +0200

    Pass test, input check, start datatype work

--- src/pymatmatmul/matmul.py
+++ src/pymatmatmul/matmul.py
@@ -1,36 +1,53 @@
 import numpy as np
+import mpi4py
 from mpi4py import MPI
 from pymatmatmul.mpi_utils import get_n_local,get_n_offset
 def matmul(A, B, n_global, m_global, p_global, algorithm="base"):
-    #TO DO: CAPIRE COME
+    ###################################
+    # Input check                     #
+    ###################################
+    # Convert as np.array
+    A = np.asarray(A,copy=None)
+    B = np.asarray(B,copy=None)
+    '''
+    https://numpy.org/doc/2.1/reference/generated/numpy.asarray.html
+    >>> a = np.array([1, 2], dtype=np.float32)
+    >>> np.shares_memory(np.asarray(a, dtype=np.float32), a)
+    True
+    >>> np.shares_memory(np.asarray(a, dtype=np.float64), a)
+    False
+    '''
+    dtype = np.promote_types(A.dtype, B.dtype)
+    #Check dtype and promote if necessary
+    if A.dtype != B.dtype:
+        A = A.astype(dtype)
+        B = B.astype(dtype)
 
-    A= np.ascontiguousarray(A)
-    B= np.ascontiguousarray(B)
-
-    #Sanitize shape
-    assert True
-
+    # Check shape, consider broadcast  if array or insert a new axis
+    if A.ndim != 2:
+        raise ValueError("A must have two dimensions")
+    if B.ndim != 2:
+        raise ValueError("B must have two dimensions")
+    # with python >= 3.10 we can move towards case switch
     if algorithm == "base":
         mm =  matmul_base
+
+    # Init mpi
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
-    n_local = get_n_local(n_global,size,rank)
-    # TO DO aggiungere dtype
-    C = np.zeros(shape=(n_local,p_global),
-                 order='C',dtype=np.double)
-    buffer = np.empty(shape=(m_global,get_n_local(p_global,size,0)),
-                      order='C',dtype=np.double)
 
     n_offset = get_n_offset(n_global, size, rank)
     n_loc = get_n_local(n_global, size, rank)
     m_offset =get_n_offset(m_global, size, rank)
     m_loc = get_n_local(m_global, size, rank)
 
-    p_loc = get_n_local(p_global, size, rank)
-    p_offset = get_n_offset(p_global, size, rank)
-    for k in range(size):
+    C = np.zeros(shape=(n_loc,p_global),
+                 order='C',dtype=dtype)
+    buffer = np.empty(shape=(m_global,get_n_local(p_global,size,0)),
+                      order='C',dtype=dtype)
 
+    for k in range(size):
 
         p_offset_iter = get_n_offset(p_global, size, k)
         p_loc_iter = get_n_local(p_global, size, k)
@@ -38,12 +55,12 @@ def matmul(A, B, n_global, m_global, p_global, algorithm="base"):
         sendcounts = np.array(comm.allgather(m_loc*p_loc_iter)) #TODO remove communication
 
         displacements = np.insert(np.cumsum(sendcounts[:-1]), 0, 0)
-        bufferino_contiguo = np.ascontiguousarray(B[0:m_loc,p_offset_iter:p_offset_iter+p_loc_iter], dtype=np.double)
+        bufferino_contiguo = np.ascontiguousarray(B[0:m_loc,p_offset_iter:p_offset_iter+p_loc_iter], dtype=dtype)
         print(f"{rank=} {bufferino_contiguo.shape=} {sendcounts=}")
 
         comm.Allgatherv(
             sendbuf=bufferino_contiguo, # TODO consider in place
-            recvbuf=(buffer, sendcounts, displacements, MPI.DOUBLE) #TODO correct data type
+            recvbuf=(buffer, sendcounts, displacements, MPI.DOUBLE  ) #TODO correct data type ->  mpi4py.util.dtlib.from_numpy_dtype(dtype)
         )
         print(f"{rank=} {buffer}")
         C[0:n_loc,p_offset_iter:p_offset_iter+p_loc_iter]=mm(A,buffer)
