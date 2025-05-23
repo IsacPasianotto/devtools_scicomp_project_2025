6da929f12b0062098cfd290289dffd66dc27c0c6
IsacPasianotto <yh4mop12z@gmail.com>
2025-05-23 10:30:03 +0200

    simpler matrix generation function

--- src/pymatmatmul/gen_matrices.py
+++ src/pymatmatmul/gen_matrices.py
@@ -1,22 +1,22 @@
 """Generate randomly matrices"""
 from typing import List, Any
 import numpy as np
+from numpy.typing import NDArray
 
 def generate_random_matrices(
         n: int,
         m: int,
-        p: int,
         generationMin: float = 0.0,
         generationMax: float = 1.0,
         dtype = np.double
-        ) -> Any:
+        ) -> NDArray:
     """
+
     Generate two random matrices A and B of dimensions n x m and m x p respectively.
 
     Args:
     - n (int): Number of rows in matrix A.
-    - m (int): Number of columns in matrix A and number of rows in matrix B.
-    - p (int): Number of columns in matrix B.
+    - m (int): Number of columns in matrix A
     - generationMin (float): Minimum value for the random number generation. Default is 0.0.
     - generationMax (float): Maximum value for the random number generation. Default is 1.0.
     - dtype (np.dtype): Type of the random number generation. Default is np.double.
@@ -25,10 +25,7 @@ def generate_random_matrices(
     Returns:
     - A Randomly generated matrix A of dimensions n x m, composed of random floats if
         backend is 'python' or numpy array if backend is 'numpy'.
-    - B Randomly generated matrix B of dimensions m x p, composed of random floats if
         backend is 'python' or numpy array if backend is 'numpy'.
     """
 
-    return ((generationMax-generationMin)*np.random.rand(n, m).astype(dtype)+generationMin,
-            (generationMax-generationMin)*np.random.rand(m, p).astype(dtype)+generationMin)
-
+    return (generationMax-generationMin)*np.random.rand(n, m).astype(dtype)+generationMin
