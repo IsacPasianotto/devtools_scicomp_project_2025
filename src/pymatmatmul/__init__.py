36b052207f864e610a018c67a9410af968d8f185
Niccolo Tosato <niccolo.tosato@yahoo.it>
2025-05-29 11:47:50 +0200

    Safer check

--- src/pymatmatmul/__init__.py
+++ src/pymatmatmul/__init__.py
@@ -3,11 +3,14 @@ import pymatmatmul.libmatmul as aotmm
 import numpy as np
 
 AOT_BACKEND_DISPATCH = dict()
-for name in dir(aotmm):
-    attr = getattr(aotmm, name)
+# analyze every function in the compiled namespace
+for function_name in dir(aotmm):
+    #given a name, get the object
+    attr = getattr(aotmm, function_name)
+    #look only at the callable
     if callable(attr):
         for d in numba_aot.supported_numba_dtypes:
-            if name.endswith(d):
+            if function_name == (f"matmul_numbaaot_{d}"):
                 AOT_BACKEND_DISPATCH[np.dtype(d)] = attr
                 break
 
